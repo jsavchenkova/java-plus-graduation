2a0f24e267ad4a8e1de11291d9e2ce33ae0d29f7
yuliya azarkovich <3tochki@gmail.com>
2025-02-06 19:34:07 +0300

    stats-server

--- stats/stats-server/src/main/java/ewm/stats/controller/StatsController.java
+++ stats/stats-server/src/main/java/ewm/stats/controller/StatsController.java
@@ -8,11 +8,7 @@ import ewm.stats.service.HitService;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
