b5193d34097266305a6705d0cef95ffd0e340863
jazarkovich <3tochki@gmail.com>
2025-03-14 23:32:53 +0300

    aggregator service

---
+++ stats/aggregator-service/src/main/java/ru/practicum/ewm/AggregatorApp.java
@@ -0,0 +1,18 @@
+package ru.practicum.ewm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.context.ConfigurableApplicationContext;
+
+@SpringBootApplication
+@ConfigurationPropertiesScan
+public class AggregatorApp {
+    public static void main (String[] args) {
+        ConfigurableApplicationContext context = SpringApplication.run(AggregatorApp.class, args);
+
+        // Получаем бин AggregationStarter из контекста и запускаем основную логику сервиса
+        AggregationStarter aggregator = context.getBean(AggregationStarter.class);
+        aggregator.start();
+    }
+}
